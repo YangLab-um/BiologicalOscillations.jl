df3b9dff840489167bb113799effab352be15332
ftavella <ftavella@umich.edu>
2023-03-27 15:52:39 -0400

    Included final velocity report on ODE equilibration

--- src/simulation.jl
+++ src/simulation.jl
@@ -16,9 +16,10 @@
 - maxiters: Number of maximum iterations for the solver. Default value 1e7
 
 # Returns
-- equilibration_data::Dict: Dictionary containing the final state of each simulation and the frequency of the equilibration time series. The output is encoded as:
+- equilibration_data::Dict: Dictionary containing the final state and velocity of each simulation as well as the frequency of the time series. The velocit is calculated as the norm of the derivative for each equation evaluated at the final equilibration state. The output is encoded as:
 ```julia
 equilibration_data = Dict('final_state' => [[final_state_pset1_var1, ..., final_state_pset1_varN], ..., [final_state_psetM_var1, ..., final_state_psetM_varN]], 
+                          'final_velocity' => [velocity_pset1, ..., velocity_psetM],
                           'frequency' => [frequency_pset1, ..., frequency_psetM])
 ```
 """
@@ -30,11 +31,15 @@ function equilibrate_ODEs(model::ReactionSystem, parameter_sets::AbstractVector,
         remake(prob, u0=initial_conditions[idx], tspan=(0.0, equilibration_times[idx]), p=parameter_sets[idx])
     end
 
-    function output_func(sol::ODESolution, ~)
+    function output_func(sol::ODESolution, i)
+        idx = Int(i)
+        ode_problem = ODEProblem(model, initial_conditions[idx], (0.0, equilibration_times[idx]), parameter_sets[idx])
         frequency_data = calculate_main_frequency(sol, length(sol.t), length(sol.t))
         mean_frequency = mean(frequency_data["frequency"])
-        endpoint = sol.u[end]
-        return ([endpoint, mean_frequency], false)
+        final_state = sol.u[end]
+        final_derivative = ode_problem.f(final_state, parameter_sets[idx], equilibration_times[idx])
+        final_velocity = sqrt(sum(final_derivative.^2))
+        return ([final_state, final_velocity, mean_frequency], false)
     end
 
     ode_problem = ODEProblem(model, initial_conditions[1], (0.0, equilibration_times[1]), parameter_sets[1])
@@ -43,7 +48,8 @@ function equilibrate_ODEs(model::ReactionSystem, parameter_sets::AbstractVector,
                        abstol=abstol, reltol=reltol, maxiters=maxiters, dense=true)
     
     equilibration_data = Dict("final_state" => [simulation.u[i][1] for i=axes(simulation.u, 1)],
-                            "frequency" => [simulation.u[i][2] for i=axes(simulation.u, 1)])
+                              "final_velocity" => [simulation.u[i][2] for i=axes(simulation.u, 1)],
+                              "frequency" => [simulation.u[i][3] for i=axes(simulation.u, 1)])
 
     return equilibration_data
 end
--- test/simulation_tests.jl
+++ test/simulation_tests.jl
@@ -14,7 +14,10 @@ initial_conditions = [0.5*ones(3) for i=1:3]
 equilibration_times = [pin_timescale(α, β, γ) for i=1:3]
 
 equilibration_data = equilibrate_ODEs(model, parameter_sets, initial_conditions, equilibration_times)
+
 @test abs(mean(equilibration_data["frequency"]) - 17.1) / 17.1 < 0.05
+@test abs(mean(equilibration_data["final_velocity"]) - 19.3) / 19.3 < 0.05
+
 @test abs(equilibration_data["final_state"][1][1] - 0.44) / 0.44 < 0.05
 @test abs(equilibration_data["final_state"][1][2] - 0.14) / 0.14 < 0.05
 @test abs(equilibration_data["final_state"][1][3] - 0.02) / 0.02 < 0.05
