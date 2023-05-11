696019b123b277138a59a929976b6270d5a0655e
ftavella <ftavella@umich.edu>
2023-05-11 11:35:04 -0400

    New functionality for calculating unique network additions

--- src/network_utilities.jl
+++ src/network_utilities.jl
@@ -25,3 +25,93 @@ function network_permutations(connectivity::AbstractMatrix)
     end
     return connectivity_vector
 end
+
+
+"""
+    is_same_network(connectivity1::AbstractMatrix, connectivity2::AbstractMatrix)
+
+    Returns true if connectiviy1 is a permutation of connectivity2. False otherwise.
+
+# Arguments (Required)
+- `connectivity1::AbstractMatrix`: Connectivity matrix of a network
+- `connectivity2::AbstractMatrix`: Connectivity matrix of a network
+
+# Returns
+- `result::Bool`: True if any of the permutations of connectivity 1 is equal to any of the permutations of connectivity2. False otherwise.
+"""
+function is_same_network(connectivity1::AbstractMatrix, connectivity2::AbstractMatrix)
+    permutations1 = network_permutations(connectivity1)
+    permutations2 = network_permutations(connectivity2)
+    # Compare permutations
+    for p1 in permutations1
+        for p2 in permutations2
+            if p1 == p2
+                return true
+            end
+        end
+    end
+    return false
+end
+
+
+"""
+    all_network_additions(connectivity::AbstractMatrix, number_of_edges::Int64)
+
+    Returns a vector containing all possible network additions of a network connectivity matrix.
+
+# Arguments (Required)
+- `connectivity::AbstractMatrix`: Connectivity matrix of a network
+- `number_of_edges::Int64`: Number of edges to add to the network
+
+# Returns
+- `connectivity_vector::AbstractVector`: Vector containing all possible network additions of a network connectivity matrix
+"""
+function all_network_additions(connectivity::AbstractMatrix, number_of_edges::Int64)
+    connectivity_vector = []
+    # Additions will only occur at places where the connectivity is zero
+    zero_indices = findall(connectivity .== 0)
+    # Get all possible combinations of signs for the additions
+    sign_combinations = reduce(vcat, collect(Iterators.product([[-1,1] for i in 1:number_of_edges]...)))
+    # Find all combinations of zero indices where number_of_edges additions will happen
+    zero_index_combinations = collect(combinations(zero_indices, number_of_edges))
+    # Create all possible network additions
+    for sign_combination in sign_combinations
+        for zero_index_combination in zero_index_combinations
+            # Create a copy of the connectivity matrix
+            new_connectivity = copy(connectivity)
+            # Add the edges
+            for (i,sign) in enumerate(sign_combination)
+                new_connectivity[zero_index_combination[i]] = sign
+            end
+            push!(connectivity_vector, new_connectivity)
+        end
+    end
+
+    return connectivity_vector
+end
+
+
+"""
+    unique_network_additions(connectivity::AbstractMatrix, number_of_edges::Int64)
+
+    Returns a vector containing all unique network additions of a network connectivity matrix.
+
+# Arguments (Required)
+- `connectivity::AbstractMatrix`: Connectivity matrix of a network
+- `number_of_edges::Int64`: Number of edges to add to the network
+
+# Returns
+- `connectivity_vector::AbstractVector`: Vector containing all unique network additions of a network connectivity matrix
+"""
+function unique_network_additions(connectivity::AbstractMatrix, number_of_edges::Int64)
+    connectivity_vector = []
+    all_additions = all_network_additions(connectivity, number_of_edges)
+    # Remove duplicates
+    for addition in all_additions
+        if !any(is_same_network(addition, c) for c in connectivity_vector)
+            push!(connectivity_vector, addition)
+        end
+    end
+
+    return connectivity_vector
+end
--- test/network_utilities_tests.jl
+++ test/network_utilities_tests.jl
@@ -11,3 +11,57 @@ true_permutations = [
 ]
 calculated_permutations = network_permutations([0 0 1; 2 0 0; 0 3 0])
 @test true_permutations == calculated_permutations
+
+# Test the is_same_network function
+connectivity_1a = [1 0 -1; -1 0 0; 0 -1 0]
+connectivity_1b = [0 0 -1; -1 1 0; 0 -1 0]
+@test is_same_network(connectivity_1a, connectivity_1b) == true
+
+connectivity_2a = [0 0 0 0 -1; 1 0 0 0 0; 0 1 0 0 0; 0 0 -1 0 0; 0 0 0 -1 0;]
+connectivity_2b = [0 0 0 0 1; -1 0 0 0 0; 0 -1 0 0 0; 0 0 -1 0 0; 0 0 0 1 0;]
+@test is_same_network(connectivity_1a, connectivity_1b) == true
+
+connectivity_3a = [0 0 0 0 0 0 -1; 1 0 0 0 0 0 0; 0 -1 0 0 0 0 0; 0 0 1 0 0 0 0; 0 0 0 -1 0 0 0; 0 0 0 0 -1 0 0; 0 0 0 0 0 -1 0;]
+connectivity_3b = [0 0 0 0 0 0 -1; 1 0 0 0 0 0 0; 0 -1 0 0 0 0 0; 0 0 -1 0 0 0 0; 0 0 0 1 0 0 0; 0 0 0 0 -1 0 0; 0 0 0 0 0 -1 0;]
+@test is_same_network(connectivity_3a, connectivity_3b) == false
+
+# Test the all_network_additions function
+base_connectivity_1 = [0 0 -1;-1 0 0; 0 -1 0]
+true_additions_1 = [
+    [1 0 -1;-1 0 0; 0 -1 0], [-1 0 -1;-1 0 0; 0 -1 0],
+    [0 1 -1;-1 0 0; 0 -1 0], [0 -1 -1;-1 0 0; 0 -1 0],
+    [0 0 -1;-1 1 0; 0 -1 0], [0 0 -1;-1 -1 0; 0 -1 0],
+    [0 0 -1;-1 0 1; 0 -1 0], [0 0 -1;-1 0 -1; 0 -1 0],
+    [0 0 -1;-1 0 0; 1 -1 0], [0 0 -1;-1 0 0; -1 -1 0],
+    [0 0 -1;-1 0 0; 0 -1 1], [0 0 -1;-1 0 0; 0 -1 -1],
+]
+
+calculated_additions_1 = all_network_additions(base_connectivity_1, 1)
+@test Set(true_additions_1) == Set(calculated_additions_1)
+
+for n in 1:6
+    true_number_of_additions = 2^n * binomial(6, n)
+    calculated_number_of_additions = length(all_network_additions(base_connectivity_1, n))
+    @test true_number_of_additions == calculated_number_of_additions
+end
+
+# Test the unique_network_additions function
+base_connectivity = [0 0 -1;-1 0 0; 0 -1 0]
+true_unique_addtions = [
+    [1 0 -1;-1 0 0; 0 -1 0], [-1 0 -1;-1 0 0; 0 -1 0],
+    [0 1 -1;-1 0 0; 0 -1 0], [0 -1 -1;-1 0 0; 0 -1 0],
+]
+calculated_unique_additions = unique_network_additions(base_connectivity, 1)
+for addition in true_unique_addtions
+    @test any([is_same_network(addition, calculated_addition) for calculated_addition in calculated_unique_additions])
+end
+
+base_connectivity = [0 0 -1;1 0 0; 0 1 0]
+true_unique_addtions = [
+    [1 0 -1;-1 0 0; 0 -1 0], [-1 0 -1;-1 0 0; 0 -1 0],
+    [0 1 -1;-1 0 0; 0 -1 0], [0 -1 -1;-1 0 0; 0 -1 0],
+]
+calculated_unique_additions = unique_network_additions(base_connectivity, 1)
+for addition in true_unique_addtions
+    @test any([is_same_network(addition, calculated_addition) for calculated_addition in calculated_unique_additions])
+end
