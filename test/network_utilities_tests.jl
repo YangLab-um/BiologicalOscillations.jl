e8c806a926fd7d64d9e24ebf64c8f53018bcd853
ftavella <ftavella@umich.edu>
2023-08-29 16:08:16 -0400

    Add extra test on network utilities

--- test/network_utilities_tests.jl
+++ test/network_utilities_tests.jl
@@ -1,6 +1,14 @@
 using BiologicalOscillations
 
-# Test that the correct network permutations are generated
+# Test `network_permutations`
+## 2 nodes
+true_permutations = [
+    [0 1; 2 0],
+    [0 2; 1 0],
+]
+calculated_permutations = network_permutations([0 1; 2 0])
+@test true_permutations == calculated_permutations
+## 3 nodes
 true_permutations = [
     [0 0 1; 2 0 0; 0 3 0],
     [0 1 0; 0 0 3; 2 0 0],
