66c30338ba2fdcfeb7628fe79e2ceda1784c74b1
ftavella <ftavella@umich.edu>
2023-10-09 16:08:38 -0400

    Add desired structure to hyperparameters to define output

--- src/default_hyperparameters.jl
+++ src/default_hyperparameters.jl
@@ -1,4 +1,63 @@
 """
+    DEFAULT_SIMULATION_OUTPUT::Dict{String, Any}
+
+Default selection of outputs for `find_pin_oscillations` and `find_grn_oscillations`.
+Each key specifies what can be saved from the following options:
+- "model": If value is set to true, saves the model that was simulated.
+- "hyperparameters": If value is set to true, saves the hyperparameters that were used in the simulation.
+- "parameter_sets": Saves the parameter sets that were simulated. A Dict is used to specify if the oscillatory and non-oscillatory parameter sets should be saved.
+- "equilibration_result": Saves data regarding the equilibration of the parameter sets. What data is saved is specified by the bool value in a Dict. For all the options see below.
+- "simulation_result": Saves data regarding the simulation of the parameter sets. What data is saved is specified by the bool value in a Dict. For all the options see below.
+
+The "equilibration_result" output can have the following options:
+- "parameter_index": Saves an identifier for each parameter set. Useful for indentifying parameter sets across result dataframes.
+- "equilibration_times": Times used for equilibrating each parameter set.
+- "final_velocity": Velocity of the final state after equilibration.
+- "final_state": Final state after equilibration. Creates one column for each species.
+- "frequency": Main frequency of the equilibration time-series. The value represents the average over all species.
+- "is_steady_state": Final decision on whether the parameter set is in steady state or not.
+
+The "simulation_result" output can have the following options:
+- "parameter_index": Saves an identifier for each parameter set. Useful for indentifying parameter sets across result dataframes.
+- "simulation_times": Times used for simulating each parameter set.
+- "final_state": Final state after simulation. Creates one column for each species.
+- "frequency": Main frequency of the simulation time-series. Creates one column for each species.
+- "fft_power": Power of the main frequency of the simulation time-series. Creates one column for each species.
+- "amplitude": Amplitude of the simulation time-series. Creates one column for each species.
+- "peak_variation": Coefficient of variation of the peak values of the simulation time-series. Creates one column for each species.
+- "trough_variation": Coefficient of variation of the trough values of the simulation time-series. Creates one column for each species.
+- "is_oscillatory": Final decision on whether the parameter set is oscillatory or not.
+"""
+DEFAULT_SIMULATION_OUTPUT = Dict(
+    "model" => true,
+    "hyperparameters" => true,
+    "parameter_sets" => Dict(
+        "oscillatory" => true,
+        "non_oscillatory" => true
+    ),
+    "equilibration_result" => Dict(
+        "parameter_index" => true,
+        "equilibration_times" => true,
+        "final_velocity" => true,
+        "final_state" => true,
+        "frequency" => true,
+        "is_steady_state" => true
+    ),
+    "simulation_result" => Dict(
+        "parameter_index" => true,
+        "simulation_times" => true,
+        "final_state" => true,
+        "frequency" => true,
+        "fft_power" => true,
+        "amplitude" => true,
+        "peak_variation" => true,
+        "trough_variation" => true,
+        "is_oscillatory" => true
+    )
+)
+
+
+"""
     DEFAULT_PIN_PARAMETER_LIMITS::Dict{String, Tuple{Float64, Float64}}
 
 Default parameter limits for the generation of pin parameter sets. Keys are the names of the parameters, values are tuples of the form (lower_limit, upper_limit).
@@ -46,7 +105,8 @@ DEFAULT_PIN_HYPERPARAMETERS = Dict(
     "simulation_time_multiplier" => 10, # Number of periods to simulate. Used in `calculate_simulation_times`
     "freq_variation_threshold" => 0.05, # Maximum tolerated variation in frequency between species in the solution to be declared oscillatory.
     "power_threshold" => 1e-7, # Minimum spectral power that the main peak has to have to be declared oscillatory.
-    "amp_variation_threshold" => 0.05 # Maximum tolerated value for peak/trough variation to be declared oscillatory.
+    "amp_variation_threshold" => 0.05, # Maximum tolerated value for peak/trough variation to be declared oscillatory.
+    "simulation_output" => DEFAULT_SIMULATION_OUTPUT,
 )
 
 
@@ -100,5 +160,6 @@ DEFAULT_GRN_HYPERPARAMETERS = Dict(
     "simulation_time_multiplier" => 10, # Number of periods to simulate. Used in `calculate_simulation_times`
     "freq_variation_threshold" => 0.05, # Maximum tolerated variation in frequency between species in the solution to be declared oscillatory.
     "power_threshold" => 1e-7, # Minimum spectral power that the main peak has to have to be declared oscillatory.
-    "amp_variation_threshold" => 0.05 # Maximum tolerated value for peak/trough variation to be declared oscillatory.
+    "amp_variation_threshold" => 0.05, # Maximum tolerated value for peak/trough variation to be declared oscillatory.
+    "simulation_output" => DEFAULT_SIMULATION_OUTPUT,
 )
